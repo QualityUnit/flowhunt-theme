7fe73538f139a87149ecc196942b9ed793b6a211
thujcak <thujcak@qualityunit.com>
2024-06-26 06:06:00 +0200

    feat: removed old useless post types

--- lib/post-types.php
+++ lib/post-types.php
@@ -1,10 +1,8 @@
 <?php
-	require_once get_template_directory() . '/lib/post-types/about.php';
+
 	require_once get_template_directory() . '/lib/post-types/features.php';
-	require_once get_template_directory() . '/lib/post-types/kb.php';
 	require_once get_template_directory() . '/lib/post-types/glossary.php';
-	require_once get_template_directory() . '/lib/post-types/guide.php';
 	require_once get_template_directory() . '/lib/post-types/pricing-tables.php';
-	require_once get_template_directory() . '/lib/post-types/videos.php';
 	require_once get_template_directory() . '/lib/post-types/checklists.php';
-	require_once get_template_directory() . '/lib/post-types/directory.php';
+	require_once get_template_directory() . '/lib/post-types/flow-components.php';
+	require_once get_template_directory() . '/lib/post-types/flow-templates.php';
--- lib/taxonomies.php
+++ lib/taxonomies.php
@@ -1,20 +1,14 @@
 <?php
 	require_once get_template_directory() . '/lib/taxonomies/features-categories.php';
 
-	//Documentation taxonomies
-	require_once get_template_directory() . '/lib/taxonomies/kb-categories.php';
-
 	//Glossary taxonomies
 	require_once get_template_directory() . '/lib/taxonomies/glossary-categories.php';
-
-	//Videos taxonomies
-	require_once get_template_directory() . '/lib/taxonomies/videos-categories.php';
-
-	// About + Contact taxonomies
-	require_once get_template_directory() . '/lib/taxonomies/about-categories.php';
 
 	// Checklists
 	require_once get_template_directory() . '/lib/taxonomies/checklists-categories.php';
 
-	// Directory
-	require_once get_template_directory() . '/lib/taxonomies/directory-categories.php';
+	// Flow components
+	require_once get_template_directory() . '/lib/taxonomies/flow-components-categories.php';
+
+	// Flow templates
+	require_once get_template_directory() . '/lib/taxonomies/flow-templates-categories.php';
