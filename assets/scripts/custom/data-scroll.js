a01193be19b1b4080a6635094b57beaaf44455fa
Jozef Remeň <jozef.remen@gmail.com>
2024-08-07 11:44:07 +0200

    fix: eslint

--- assets/scripts/custom/data-scroll.js
+++ assets/scripts/custom/data-scroll.js
@@ -66,7 +66,7 @@ if ( scrollByPoint.length > 0 ) {
 				end: `+=${ ( details.length ) * 100 }%`,
 				pin: true,
 				scrub: 1,
-				invalidateOnRefresh: true
+				invalidateOnRefresh: true,
 			} );
 		} );
 
