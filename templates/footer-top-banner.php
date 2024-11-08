646fc843c344ecec66ecb5b9e5178c39585fb21f
Jozef Remeň <jozef.remen@gmail.com>
2024-11-08 09:27:51 +0100

    feat: icons, top banner

--- templates/footer-top-banner.php
+++ templates/footer-top-banner.php
@@ -6,10 +6,10 @@
       <h2 class="Footer__TopBanner__title"><?php _e( 'Ready to build your own AI?', 'flowhunt' ); ?></h2>
       <p class="Footer__TopBanner__subtitle"><?php _e( 'Smart Chatbots and AI tools under one roof. Connect intuitive blocks to turn your ideas into automated Flows.', 'flowhunt' ); ?></p>
       <div class="Footer__TopBanner__buttons">
-        <a class="Button Button--white Button--medium" href="<?php _e( 'https://app.flowhunt.io/sign-in', 'flowhunt' ); ?>" target="_blank">
+        <a class="Button Button--white Button--white--highlight Button--medium" href="<?php _e( 'https://app.flowhunt.io/sign-in', 'flowhunt' ); ?>" target="_blank">
           <span><?php _e( 'Try it out', 'flowhunt' ); ?></span>
         </a>
-        <a class="Button Button--outline--white Button--medium" href="<?php _e( 'https://calendly.com/liveagentsession/flowhunt-chatbot-demo', 'flowhunt' ); ?>" target="_blank">
+        <a class="Button Button--outline--white Button--outline--full Button--medium" href="<?php _e( 'https://calendly.com/liveagentsession/flowhunt-chatbot-demo', 'flowhunt' ); ?>" target="_blank">
           <span><?php _e( 'Book a demo', 'flowhunt' ); ?></span>
         </a>
       </div>
@@ -23,25 +23,26 @@
         <p class="Footer__TopBanner__banner--subtitle"><?php _e( 'Connect intuitive blocks to turn your ideas and needs into automated Flows', 'flowhunt' ); ?></p>
       </div>
       <div class="Footer__TopBanner__banner--social">
-								<a href="<?php the_author_meta( 'instagram' ); ?>" target="_blank" itemprop="sameAs">
-									<i class="fontello-instagram-brands"></i>
-								</a>
-							<?php } ?>
-							<?php if ( (bool) get_the_author_meta( 'facebook' ) ) { ?>
-								<a href="<?php the_author_meta( 'facebook' ); ?>" target="_blank" itemprop="sameAs">
-									<i class="fontello-facebook-f-brands"></i>
-								</a>
-							<?php } ?>
-							<?php if ( (bool) get_the_author_meta( 'linkedin' ) ) { ?>
-								<a href="<?php the_author_meta( 'linkedin' ); ?>" target="_blank" itemprop="sameAs">
-									<i class="fontello-linkedin-in-brands"></i>
-								</a>
-							<?php } ?>
-							<?php if ( (bool) get_the_author_meta( 'twitter' ) ) { ?>
-								<a href="https://twitter.com/<?php the_author_meta( 'twitter' ); ?>" target="_blank" itemprop="sameAs">
-									<i class="fontello-twitter-brands"></i>
-								</a>
-							<?php } ?>
+        <a href="<?php the_author_meta( 'instagram' ); ?>" target="_blank" itemprop="sameAs">
+          <svg class="icon icon-social-instagram">
+            <use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg#social-instagram' ); ?>"></use>
+          </svg>
+        </a>
+        <a href="<?php the_author_meta( 'facebook' ); ?>" target="_blank" itemprop="sameAs">
+          <svg class="icon icon-social-facebook">
+            <use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg#social-facebook' ); ?>"></use>
+          </svg>
+        </a>
+        <a href="https://twitter.com/<?php the_author_meta( 'twitter' ); ?>" target="_blank" itemprop="sameAs">
+          <svg class="icon icon-social-twitter">
+            <use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg#social-twitter' ); ?>"></use>
+          </svg>
+        </a>
+        <a href="<?php the_author_meta( 'linkedin' ); ?>" target="_blank" itemprop="sameAs">
+          <svg class="icon icon-social-linkedin">
+            <use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg#social-linkedin' ); ?>"></use>
+          </svg>
+        </a>
       </div>
     </div>
   </div>
