6605a57c6c78c4c0f8be17fa35268037ab28877d
Jozef Remen <jozef.remen@gmail.com>
2024-11-15 09:45:33 +0100

    fix: table javascript (#155)
    * fix: table javascript
    
    * fix: link css

--- assets/scripts/custom/responsiveTable.js
+++ assets/scripts/custom/responsiveTable.js
@@ -46,7 +46,7 @@ if ( tables.length ) {
 
 					if ( hasTooltip.test( text ) ) {
 						const infoIcon = `<svg class="icon icon-info-circle">
-							<use xlink:href="/app/themes/flowhunt-theme/assets/images/icons.svg#info-circle"></use>
+							<use xlink:href="/app/themes/flowhunt/assets/images/icons.svg#info-circle"></use>
 							</svg>`;
 						cell.classList.add( 'hasTooltip' );
 						cell.innerHTML = text.replaceAll( hasTooltip, `$1<div class="ComparePlans__tooltip">${ infoIcon }<span class="ComparePlans__tooltip__text">$2</span></div>` );
@@ -61,7 +61,7 @@ if ( tables.length ) {
 						val.classList.add( 'icn-after-check' );
 						val.insertAdjacentHTML( 'afterbegin', `
 							<svg class="icon icon-check">
-								<use xlink:href="/app/themes/flowhunt-theme/assets/images/icons.svg#check"></use>
+								<use xlink:href="/app/themes/flowhunt/assets/images/icons.svg#check"></use>
 							</svg>`
 						);
 					}
@@ -70,7 +70,7 @@ if ( tables.length ) {
 						val.classList.add( 'icn-after-close' );
 						val.insertAdjacentHTML( 'afterbegin', `
 							<svg class="icon icon-close">
-								<use xlink:href="/app/themes/flowhunt-theme/assets/images/icons.svg#close"></use>
+								<use xlink:href="/app/themes/flowhunt/assets/images/icons.svg#close"></use>
 							</svg>`
 						);
 					}
