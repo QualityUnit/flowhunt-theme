4ae74011ec1a2d658c0ca1444487ab4fd2526f2b
Jozef Remeň <jozef.remen@gmail.com>
2024-12-04 11:24:32 +0100

    fix: small fixes

--- lib/components/post-sidebar.php
+++ lib/components/post-sidebar.php
@@ -20,27 +20,25 @@
 	  </p>
 	  <a class="Button Button--full pt-s pb-s" href="<?= esc_url( '#0' ); ?>" target="_blank"><span><?= esc_html( 'Get started for FREE', 'flowhunt' ); ?></span></a>
 	</div>
-	<div class="Post__sidebar--Share">
-	  <h4><?php _e( 'Share this article on', 'flowhunt' ); ?></h4>
-		<div class="Post__sidebar--Share__items flex">
-			<div class="Post__sidebar--Share__item">
-				<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>" target="_blank" itemprop="sameAs"
-					title="<?php _e( 'Share on', 'ms' ); ?> <?php _e( 'LinkedIn', 'ms' ); ?>">
-					<svg class='icon icon-linkedin'  style="fill:rgb(10,102,194);" ><use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg?ver=' . THEME_VERSION . '#social-linkedin' ); ?>"></use></svg>
-				</a>
-			</div>
-			<div class="Post__sidebar--Share__item">
-				<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank" itemprop="sameAs"
-					title="<?php _e( 'Share on', 'ms' ); ?> <?php _e( 'Facebook', 'ms' ); ?>">
-					<svg class='icon icon-facebook' style="fill:rgb(8,102,255);"><use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg?ver=' . THEME_VERSION . '#social-facebook' ); ?>"></use></svg>
-				</a>
-			</div>
-			<div class="Post__sidebar--Share__item">
-				<a href="https://twitter.com/share?url=<?php the_permalink(); ?>" target="_blank" itemprop="sameAs"
-					title="<?php _e( 'Share on', 'ms' ); ?> <?php _e( 'X', 'ms' ); ?>">
-					<svg class='icon icon-x'><use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg?ver=' . THEME_VERSION . '#social-x' ); ?>"></use></svg>
-				</a>
-			</div>
+	<div class="Post__sidebar--Share flex flex-align-center">
+		<span><?php _e( 'Share this article on', 'flowhunt' ); ?></span>
+		<div class="Post__sidebar--Share__item">
+			<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>" target="_blank" itemprop="sameAs"
+				title="<?php _e( 'Share on', 'ms' ); ?> <?php _e( 'LinkedIn', 'ms' ); ?>">
+				<svg class='icon icon-linkedin'><use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg?ver=' . THEME_VERSION . '#social-linkedin' ); ?>"></use></svg>
+			</a>
+		</div>
+		<div class="Post__sidebar--Share__item">
+			<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank" itemprop="sameAs"
+				title="<?php _e( 'Share on', 'ms' ); ?> <?php _e( 'Facebook', 'ms' ); ?>">
+				<svg class='icon icon-facebook'><use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg?ver=' . THEME_VERSION . '#social-facebook' ); ?>"></use></svg>
+			</a>
+		</div>
+		<div class="Post__sidebar--Share__item">
+			<a href="https://twitter.com/share?url=<?php the_permalink(); ?>" target="_blank" itemprop="sameAs"
+				title="<?php _e( 'Share on', 'ms' ); ?> <?php _e( 'X', 'ms' ); ?>">
+				<svg class='icon icon-x'><use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg?ver=' . THEME_VERSION . '#social-x' ); ?>"></use></svg>
+			</a>
 		</div>
 	</div>
   </div>
